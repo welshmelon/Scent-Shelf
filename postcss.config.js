7ae2829653009371d1b754588b3cc0f1342b8bd5
welshmelon <helencook@hotmail.com>
2025-07-13 18:50:33 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-// FILE 6: postcss.config.js
-// Another configuration file for styling.
-// Create this file in the main directory of your repository.
-// Name the file postcss.config.js
-
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-```
