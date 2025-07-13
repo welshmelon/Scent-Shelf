3d9087103aa9a66a41c497388926a2189feede13
welshmelon <helencook@hotmail.com>
2025-07-13 18:24:56 +0100

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+// FILE 6: postcss.config.js
+// Another configuration file for styling.
+// Create this file in the main directory of your repository.
+// Name the file postcss.config.js
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// FILE 7: tailwind.config.js
+// The last configuration file for styling.
+// Create this file in the main directory of your repository.
+// Name the file tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
