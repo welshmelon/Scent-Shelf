2d39b4ba88175b5420a0234abdf07bb2ef82897e
welshmelon <helencook@hotmail.com>
2025-07-13 18:51:09 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,3 @@
-// FILE 7: tailwind.config.js
-// The last configuration file for styling.
-// Create this file in the main directory of your repository.
-// Name the file tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
