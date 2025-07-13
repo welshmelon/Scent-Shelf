19843e19caac96a6f565f4adcff640301b42cf8d
welshmelon <helencook@hotmail.com>
2025-07-13 18:54:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App' // The .js extension has been removed to let the build tool find the file.
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
