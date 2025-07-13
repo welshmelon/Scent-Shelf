8d028111085cf7510b07f45ebbf1e2cb59a8fda7
welshmelon <helencook@hotmail.com>
2025-07-13 18:51:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
-// FILE 5: vite.config.js
-// This is a configuration file for the build tool.
-// Create this file in the main directory of your repository.
-// Name the file vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -10,4 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-```
